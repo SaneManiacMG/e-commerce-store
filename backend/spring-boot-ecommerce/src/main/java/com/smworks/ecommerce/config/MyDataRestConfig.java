b58bbd1b917402b77fada48ba9cbe68b91ac5ecf
Mogomotsi Moroane <mmoroane@hotmail.com>
2022-06-07 17:33:44 +0200

    Create MyDataRestConfig.java
    Making product table readonly

---
+++ backend/spring-boot-ecommerce/src/main/java/com/smworks/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,23 @@
+package com.smworks.ecommerce.config;
+
+import com.smworks.ecommerce.entity.Product;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
