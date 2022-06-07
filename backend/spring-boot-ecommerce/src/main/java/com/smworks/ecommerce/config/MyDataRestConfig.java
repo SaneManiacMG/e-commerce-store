609f5ae35f13924da195f659a43e23c0685a5594
Mogomotsi Moroane <mmoroane@hotmail.com>
2022-06-07 17:34:49 +0200

    Update MyDataRestConfig.java
    Disabled writing and updating for product category

--- backend/spring-boot-ecommerce/src/main/java/com/smworks/ecommerce/config/MyDataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/smworks/ecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.smworks.ecommerce.config;
 
 import com.smworks.ecommerce.entity.Product;
+import com.smworks.ecommerce.entity.ProductCategory;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
@@ -17,6 +18,12 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // disable HTTP methods for Product: PUT, POST, DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
+                .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for Product Category: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
                 .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withAssociationExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
