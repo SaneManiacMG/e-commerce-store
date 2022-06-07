bc628e4690fe30fbf31010cf031e653dbccea572
Mogomotsi Moroane <mmoroane@hotmail.com>
2022-06-07 17:35:33 +0200

    Update MyDataRestConfig.java
    @Configuration for spring scanning

--- backend/spring-boot-ecommerce/src/main/java/com/smworks/ecommerce/config/MyDataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/smworks/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,13 @@ package com.smworks.ecommerce.config;
 
 import com.smworks.ecommerce.entity.Product;
 import com.smworks.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
